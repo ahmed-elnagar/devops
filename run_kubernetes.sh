e780f21b64ac423259c2ebf39b3d0aea5d0b27d7
ahmed-elnagar <55024226+ahmed-elnagar@users.noreply.github.com>
2020-10-04 17:54:01 +0200

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,12 +5,13 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=elnaggar3012/devops-proj4
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run proj4 --generator=run-pod/v1 --image=$dockerpath --port=80
 # Step 3:
 # List kubernetes pods
 kubectl list pods
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/proj4 8000:80
